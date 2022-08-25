4be603630688b40e425c78871d6c27a961e1f411
geossophia <geossophia@gmail.com>
2022-08-25 11:25:14 -0300

    aula2

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,29 @@ const preço = ref("");
 const gorgeta = ref("");
 </script>
 <template>
-  <div class="inputs">
-    <input type="text" v-model="preço" placeholder="Valor da conta" />
+  <div class="outer">
+    <h1>Calculadora de Gorjeta</h1>
+    <div class="inputs">
+      <div>
+        <input type="text" v-model="preço" placeholder="Valor da conta" />
+      </div>
 
-    <select name="Qualidade de serviço" id="asd" v-model="gorgeta">
-    <option value="" disable>Porcentagem</option>
-      <option value="5">5%</option>
-      <option value="10">10%</option>
-      <option value="15">15%</option>
-      <option value="20">20%</option>
-      <option value="25">25%</option>
-    </select>
+      <select name="Qualidade de serviço" id="asd" v-model="gorgeta">
+        <option value="" disable>Porcentagem</option>
+        <option value="5">5%</option>
+        <option value="10">10%</option>
+        <option value="15">15%</option>
+        <option value="20">20%</option>
+        <option value="25">25%</option>
+      </select>
+      <div>
+      <input type="text" placeholder="Quantidade de amigos" v-model="pessoas" />
+      </div>
 
-    <input type="text" placeholder="Quantidade de amigos" v-model="pessoas" />
-
-    <p>
-      Valor para cada um pagar: R$ {{ (preço * (1 + gorgeta / 100)) / pessoas }}
-    </p>
+      <p>
+        Valor para cada um pagar: R$
+        {{ (preço * (1 + gorgeta / 100)) / pessoas }}
+      </p>
+    </div>
   </div>
 </template>
